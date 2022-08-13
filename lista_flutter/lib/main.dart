014293a6af85e57ea81ddeda53944a8fd9293ee5
AllysonFernando <allyson.fernand@gmail.com>
2022-08-13 10:11:49 -0300

    Add Formulario

--- lista_flutter/lib/main.dart
+++ lista_flutter/lib/main.dart
@@ -1,52 +1,94 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaDeCompras(),
-          appBar: AppBar(
-              title: const Text(
-            "Lista de Compras",
-            textAlign: TextAlign.center,
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => {},
-            child: const Icon(Icons.add_shopping_cart),
-          ),
-        ),
-      ),
-    );
+void main() => runApp(ListaDeComprasApp());
 
-class ListaDeCompras extends StatelessWidget {
-  const ListaDeCompras({Key? key}) : super(key: key);
+class ListaDeComprasApp extends StatelessWidget {
+  const ListaDeComprasApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        AdicionarItem(item: "Arroz", quantidade: "2"),
-        AdicionarItem(item: "Bolacha", quantidade: "1")
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioCompra(),
+      ),
     );
   }
 }
 
-class AdicionarItem extends StatelessWidget {
-  final String item;
-  final String quantidade;
+class ListaDeCompras extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        "Lista de Compras",
+        textAlign: TextAlign.center,
+      )),
+      body: Column(
+        children: <Widget>[
+          ItensAdicionado(AdicionarItem("Arroz", 2)),
+          ItensAdicionado(AdicionarItem("Arroz", 2)),
+          ItensAdicionado(AdicionarItem("Arroz", 2)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: const Icon(Icons.add_shopping_cart),
+      ),
+    );
+  }
+}
 
-  const AdicionarItem(
-      {super.key, required this.item, required this.quantidade});
+class ItensAdicionado extends StatelessWidget {
+  final AdicionarItem _adicionarItem;
+
+  ItensAdicionado(this._adicionarItem);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: const Icon(Icons.shopping_cart),
-        title: Text(item),
-        subtitle: Text("Qtd: $quantidade"),
+        title: Text(_adicionarItem.item.toString()),
+        subtitle: Text('Qtd: ${_adicionarItem.quantidade.toString()}'),
+      ),
+    );
+  }
+}
+
+class AdicionarItem {
+  final String item;
+  final int quantidade;
+
+  AdicionarItem(this.item, this.quantidade);
+}
+
+class FormularioCompra extends StatelessWidget {
+  const FormularioCompra({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Adicionar Itens")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              style: TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                labelText: "Nome",
+                hintText: "Ex. Arroz",
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Quantidade",
+              ),
+            ),
+            ElevatedButton(onPressed: () => {}, child: Text("Confirmar"))
+          ],
+        ),
       ),
     );
   }
